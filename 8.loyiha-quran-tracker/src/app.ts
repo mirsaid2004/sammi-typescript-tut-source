f8403bdaf67e4eafa04d74d7afde6f03e90ab6c7
Mirsaid <mir21.07.2004@gmail.com>
2025-08-18 11:44:17 +0500

    update: setting up quran tracker app tsconfig and package.json

--- 8.loyiha-quran-tracker/src/app.ts
+++ 8.loyiha-quran-tracker/src/app.ts
@@ -1,5 +1,15 @@
+import inquirer from 'inquirer';
+
 async function bootstrap() {
     console.log("Bootstraping Application...");
+    
+    await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+    ]);
 }
 
 bootstrap()
